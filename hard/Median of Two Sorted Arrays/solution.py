dc3039b916a991c96a1aa94a843a57ee1bc51694
red_byte <developer.sokolov@gmail.com>
2021-03-23 00:45:06 +0300

    @solution: find median sorted arrays

--- hard/Median of Two Sorted Arrays/solution.py
+++ hard/Median of Two Sorted Arrays/solution.py
@@ -4,15 +4,27 @@ from typing import List
 class Solution:
 
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = list(set(nums1 + nums2))
-        size = len(merged)
-        if size % 2 != 0:
-            result = merged[int(size / 2)]
+        def median(x):
+            n = len(x)
+            if n % 2 == 0:
+                median1 = x[n // 2]
+                median2 = x[n // 2 - 1]
+                med = (median1 + median2) / 2
+            else:
+                med = x[n // 2]
+            return med
+
+        if not nums1:
+            return median(nums2)
+        elif not nums2:
+            return median(nums1)
         else:
-            right = merged[int(size / 2)]
-            left = merged[int(size / 2) - 1]
-            result = (left + right) / 2
-        return float("{:.4f}".format(float(result)))
+            if not all(nums1) and not all(nums2):
+                return 0.00000
+            else:
+                nums1.extend(nums2)
+                nums1.sort()
+                return median(nums1)
 
 
 s = Solution()
