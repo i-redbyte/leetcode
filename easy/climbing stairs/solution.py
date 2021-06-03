742066a266657b318e49b41e6406bccaeea50e82
Ilya Sokolov <developer.sokolov@gmail.com>
2021-06-04 01:18:20 +0300

    @solution: Climbing Stairs (added 2 solution)

--- easy/climbing stairs/solution.py
+++ easy/climbing stairs/solution.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2:
@@ -11,10 +14,26 @@ class Solution:
             c = tmp
         return result
 
+    # Solution 2
+    # Use  Fibonacci Formula
+    def climbStairs2(self, n: int) -> int:
+        s = math.sqrt(5)
+        n_fib = pow((1 + s) / 2, n + 1) - pow((1 - s) / 2, n + 1)
+        return int(n_fib / s)
+
 
 s = Solution()
-print(s.climbStairs(2))
-print(s.climbStairs(3))
-print(s.climbStairs(4))
-print(s.climbStairs(10))
-print(s.climbStairs(45))
+# print(s.climbStairs(2))
+# print(s.climbStairs(3))
+# print(s.climbStairs(4))
+# print(s.climbStairs(10))
+# print(s.climbStairs(45))
+
+print(s.climbStairs2(2))
+print(s.climbStairs2(3))
+print(s.climbStairs2(4))
+print(s.climbStairs2(5))
+print(s.climbStairs2(6))
+print(s.climbStairs2(9))
+print(s.climbStairs2(10))
+print(s.climbStairs2(45))
