f2d3ae33061ff255b321b6cf62addb64b1b31fb6
Ilya Sokolov <developer.sokolov@gmail.com>
2021-06-18 00:14:44 +0300

    @solution: Missing Number (second solution)

--- easy/missing number/solution.py
+++ easy/missing number/solution.py
@@ -9,9 +9,23 @@ class Solution:
             result ^= i ^ nums[i]
         return result
 
+    # with Gauss's Formula; See: https://brilliant.org/wiki/sum-of-n-n2-or-n3/
+    def missingNumber2(self, nums: List[int]) -> int:
+        k = len(nums) * (len(nums) + 1) // 2
+        s = 0
+        for i in nums:
+            s += i
+        return k - s
+
 
 s = Solution()
 print(s.missingNumber([3, 0, 1]))
 print(s.missingNumber([0, 1]))
 print(s.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
 print(s.missingNumber([0]))
+# 2 solution
+print("---Two solution!---")
+print(s.missingNumber2([3, 0, 1]))
+print(s.missingNumber2([0, 1]))
+print(s.missingNumber2([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+print(s.missingNumber2([0]))
