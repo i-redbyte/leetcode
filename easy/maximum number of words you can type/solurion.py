b8273c3a9bbdef76c9a16098cae96ae236d5690b
Ilya Sokolov <developer.sokolov@gmail.com>
2021-08-26 02:03:53 +0300

    @solution: Maximum Number of Words You Can Type

---
+++ easy/maximum number of words you can type/solurion.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        result = 0
+        words = text.split(" ")
+        for i in words:
+            set_word = set(i)
+            sub = set_word - set(brokenLetters)
+            if len(set_word) == len(sub):
+                result += 1
+        return result
+
+
+s = Solution()
+print(s.canBeTypedWords("hello world", "ad"))
+print(s.canBeTypedWords("leet code", "lt"))
+print(s.canBeTypedWords("leet code", "e"))
+print(s.canBeTypedWords("assembly is the best", "z"))
