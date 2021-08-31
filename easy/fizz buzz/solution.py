9e2a8aef341579477669dade50f137275a2cbfd1
Ilya Sokolov <developer.sokolov@gmail.com>
2021-09-01 00:10:22 +0300

    @solution: second solution "Fizz Buzz" task

--- easy/fizz buzz/solution.py
+++ easy/fizz buzz/solution.py
@@ -4,12 +4,20 @@ from typing import List
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         result = []
-        if n < 1:
-            return result
-        if n <= 2:
-            for i in range(1, n + 1):
-                result.append(str(i))
-            return result
+        fizzbuzz_dict = {3: "Fizz", 5: "Buzz"}
+        n += 1
+        for num in range(1, n):
+            tmp = ""
+            for key in fizzbuzz_dict.keys():
+                if num % key == 0:
+                    tmp += fizzbuzz_dict[key]
+            if tmp == "":
+                tmp = str(num)
+            result.append(tmp)
+        return result
+
+    def fizzBuzz2(self, n: int) -> List[str]:
+        result = []
         n += 1
         for i in range(1, n):
             if i % 3 == 0 and i % 5 == 0:
@@ -29,3 +37,7 @@ print(s.fizzBuzz(2))
 print(s.fizzBuzz(3))
 print(s.fizzBuzz(5))
 print(s.fizzBuzz(15))
+print(s.fizzBuzz2(15))
+print(s.fizzBuzz2(1))
+print(s.fizzBuzz2(2))
+print(s.fizzBuzz2(0))
