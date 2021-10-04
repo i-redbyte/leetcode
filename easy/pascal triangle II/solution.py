1d49868f09a0d037ce0df5813c799a495b3a8671
Ilya Sokolov <developer.sokolov@gmail.com>
2021-10-04 23:25:43 +0300

    @solution: Pascal's Triangle II (recursive variant)

--- easy/pascal triangle II/solution.py
+++ easy/pascal triangle II/solution.py
@@ -2,17 +2,31 @@ from typing import List
 
 
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
+    def getRow2(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        result = []
+        temp = self.getRow2(rowIndex - 1)
+        n = len(temp)
+        for i in range(n - 1):
+            result.append(temp[i] + temp[i + 1])
+        return [1] + result + [1]
+
+    def getRow1(self, rowIndex: int) -> List[int]:
         row = [1]
         triangle = []
-        for i in range(34):
+        for i in range(rowIndex + 1):
             triangle.append(row)
             row = [sum(x) for x in zip([0] + row, row + [0])]
         return triangle[rowIndex]
 
 
 s = Solution()
-print(s.getRow(3))
-print(s.getRow(0))
-print(s.getRow(1))
-print(s.getRow(33))
+print(s.getRow1(3))
+print(s.getRow1(0))
+print(s.getRow1(1))
+print(s.getRow1(33))
+print(s.getRow2(3))
+print(s.getRow2(0))
+print(s.getRow2(1))
+print(s.getRow2(33))
