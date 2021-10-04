6771f51c6ff4690493f5f0d6c5ef99968a0684d6
Ilya Sokolov <developer.sokolov@gmail.com>
2021-10-05 01:07:31 +0300

    @solution: Check If String Is a Prefix of Array

---
+++ easy/check if string Is a prefix of array/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        pos = 0
+        n = len(s)
+        for word in words:
+            len_word = len(word)
+            if pos >= n:
+                return True
+            if s[pos:len_word + pos] != word:
+                return False
+            pos += len_word
+        return pos >= n
+
+
+s = Solution()
+print(s.isPrefixString("iloveleetcode", ["i", "love", "leetcode", "apples"]))
+print(s.isPrefixString("iloveleetcode", ["apples", "i", "love", "leetcode"]))
+print(s.isPrefixString("a", ["aa", "aaaa", "banana"]))
+print(s.isPrefixString("ccccccccc", ["c", "cc"]))
