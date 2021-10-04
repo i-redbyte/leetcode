02e29e9e06a8f767dda3fcd07cc8691992835c43
Ilya Sokolov <developer.sokolov@gmail.com>
2021-10-05 00:35:55 +0300

    @solution: Sort Array By Parity(two solution)

--- easy/sort array by parity/solution.py
+++ easy/sort array by parity/solution.py
@@ -2,7 +2,19 @@ from typing import List
 
 
 class Solution:
+
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        i, k = 0, len(nums) - 1
+        while i < k:
+            if nums[i] & 1 > nums[k] & 1:
+                nums[i], nums[k] = nums[k], nums[i]
+            if nums[i] & 1 == 0:
+                i += 1
+            if nums[k] & 1 == 1:
+                k -= 1
+        return nums
+
+    def sortArrayByParity1(self, nums: List[int]) -> List[int]:
         n = len(nums)
         result = []
         for i in nums:
