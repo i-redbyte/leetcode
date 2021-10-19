07c945ef5a660bbda3f4228a9463cbb65eb14890
Ilya Sokolov <developer.sokolov@gmail.com>
2021-10-19 23:14:20 +0300

    @solution: First Unique Character in a String (third solution)

--- easy/first unique character in a string/solution.py
+++ easy/first unique character in a string/solution.py
@@ -2,7 +2,14 @@ import collections
 
 
 class Solution:
+
     def firstUniqChar(self, s: str) -> int:
+        for i in range(len(s)):
+            if (s[i] not in s[:i]) and (s[i] not in s[i + 1:]):
+                return i
+        return -1
+
+    def firstUniqChar2(self, s: str) -> int:
         counter = collections.Counter(s)
         for (i, c) in enumerate(s):
             if counter[c] == 1:
