5dd0549dc85d0ed5e302d9e06bd48a55b69ab241
Ilya Sokolov <developer.sokolov@gmail.com>
2021-10-21 01:52:27 +0300

    @solution: Reverse Words in a String (second solution)

--- medium/reverse words in a string/solution.py
+++ medium/reverse words in a string/solution.py
@@ -1,5 +1,14 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        s = " ".join(s.split())
+        words = s.split(" ")
+        n = len(words)
+        m = n // 2
+        for i in range(m):
+            words[i], words[n - i - 1] = words[n - i - 1], words[i]
+        return " ".join(words)
+
+    def reverseWords1(self, s: str) -> str:
         return " ".join(s.split()[::-1])
 
 
