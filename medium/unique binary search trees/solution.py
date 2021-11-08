d82a9d942f91683e1b885a61a65bb8602df52cec
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-09 00:51:46 +0300

    @solution: Unique Binary Search Trees (second solution)

--- medium/unique binary search trees/solution.py
+++ medium/unique binary search trees/solution.py
@@ -1,10 +1,20 @@
 class Solution:
+    def numTrees(self, n: int) -> int:
+        if n == 1:
+            return 1
+        v = [0] * (n + 1)
+        v[0], v[1], v[2] = 1, 1, 2
+        for i in range(2, n):
+            for j in range(0, i+1):
+                v[i + 1] += v[j] * v[i - j]
+        return v[n]
+
     def fak(self, n):
         if n == 1:
             return 1
         return self.fak(n - 1) * n
 
-    def numTrees(self, n: int) -> int:
+    def numTrees1(self, n: int) -> int:
         return self.fak((2 * n)) // (self.fak(n) * self.fak(n + 1))
 
 
