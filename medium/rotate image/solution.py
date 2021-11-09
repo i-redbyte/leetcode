f38128ef6df8903e47c8c93328b9f63683f25ebb
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-10 00:13:00 +0300

    @solution: Rotate Image

---
+++ medium/rotate image/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])
+        row = n // 2
+        col = row + n % 2
+        for i in range(col):
+            for j in range(row):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+        print(matrix)  # for test
+
+
+s = Solution()
+
+s.rotate([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]])
