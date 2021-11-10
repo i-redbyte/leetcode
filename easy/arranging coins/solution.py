a3c4ae6b00d16e6ffed1ea642e605ee8ed890634
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-11 00:29:21 +0300

    @solution: Arranging Coins (second solution)

--- easy/arranging coins/solution.py
+++ easy/arranging coins/solution.py
@@ -1,5 +1,11 @@
+from math import sqrt
+
+
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+        return int((sqrt(1 + (8 * n)) - 1) / 2)
+
+    def arrangeCoins1(self, n: int) -> int:
         result = 0
         step = 1
         while n >= 0:
