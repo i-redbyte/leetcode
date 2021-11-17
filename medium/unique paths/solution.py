2d12414d1e2c094fdbcae543f3d66c1d6146c105
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-17 14:37:11 +0300

    @solution: Unique Paths (second solution)

--- medium/unique paths/solution.py
+++ medium/unique paths/solution.py
@@ -5,6 +5,15 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         if not m or not n:
             return 0
+        current = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                current[j] += current[j - 1]
+        return current[-1]
+
+    def uniquePaths1(self, m: int, n: int) -> int:
+        if not m or not n:
+            return 0
         return int(math.factorial(m + n - 2) / (math.factorial(n - 1) * math.factorial(m - 1)))
 
 
