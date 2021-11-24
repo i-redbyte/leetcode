68fb68096dba118c4c1830b88acf133683852ea5
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-25 00:56:01 +0300

    @feat: Same tree (second solution)

--- easy/same tree/solution.py
+++ easy/same tree/solution.py
@@ -11,6 +11,11 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+
+    def isSameTree1(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
         if not q or not p:
