4dd1b5faeb5020dd05904ed7309450701c2f675e
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-05 19:21:15 +0300

    @solution: House Robber III

---
+++ medium/house robber III/solution.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def dfs(self, root: Optional[TreeNode]) -> (int, int):
+        if not root:
+            return 0, 0
+        amount_left, max_amount_left = self.dfs(root.left)
+        amount_right, max_amount_right = self.dfs(root.right)
+        amount = max_amount_left + max_amount_right + root.val
+        max_amount = max(amount_left, max_amount_left) + max(amount_right, max_amount_right)
+        return amount, max_amount
+
+    def rob(self, root: Optional[TreeNode]) -> int:
+        pick, nopick = self.dfs(root)
+        return max(pick, nopick)
+
+
+s = Solution()
+print(s.rob(TreeNode(3, TreeNode(2, None, TreeNode(3)), TreeNode(3, None, TreeNode(1)))))
