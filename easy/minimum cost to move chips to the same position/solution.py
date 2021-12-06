6a99f14d6d26c793ee7a8d15c09dd5f17ef42394
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-06 14:35:19 +0300

    @solution: Minimum Cost to Move Chips to The Same Position

---
+++ easy/minimum cost to move chips to the same position/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even_step_counter = 0
+        odd_step_counter = 0
+        for i in position:
+            if i & 1 == 0:
+                even_step_counter += 1
+            else:
+                odd_step_counter += 1
+        return min(even_step_counter, odd_step_counter)
+
+
+s = Solution()
+print(s.minCostToMoveChips([1, 2, 3]))
+print(s.minCostToMoveChips([2, 2, 2, 3, 3]))
+print(s.minCostToMoveChips([1, 1000000000]))
