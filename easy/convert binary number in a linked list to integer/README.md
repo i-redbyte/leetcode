323fab445578dea4b324f7ba12aaa1965ca1330d
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-07 13:14:51 +0300

    @solution: Convert Binary Number in a Linked List to Integer

---
+++ easy/convert binary number in a linked list to integer/README.md
@@ -0,0 +1,51 @@
+# Convert Binary Number in a Linked List to Integer
+
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+Example 1:
+
+![](https://assets.leetcode.com/uploads/2019/12/05/graph-1.png)
+
+```
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+```
+
+Example 2:
+
+```
+Input: head = [0]
+Output: 0
+```
+
+Example 3:
+
+```
+Input: head = [1]
+Output: 1
+```
+
+Example 4:
+
+```
+Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+Output: 18880
+```
+
+Example 5:
+
+```
+Input: head = [0,0]
+Output: 0
+```
+
+**Constraints:**
+
+- The Linked List is not empty.
+- Number of nodes will not exceed 30.
+- Each node's value is either 0 or 1.
+
+[Link](https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/)
---
+++ easy/convert binary number in a linked list to integer/solution.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        binaries = []
+        while head:
+            binaries.append(head.val)
+            head = head.next
+        n = len(binaries) - 1
+        result = 0
+        p = 0
+        for i in range(n, -1, -1):
+            result += pow(2, p) * binaries[i]
+            p += 1
+        return result
+
+
+s = Solution()
+print(s.getDecimalValue(ListNode(1, ListNode(0, ListNode(1)))))
+print(s.getDecimalValue(ListNode(1, ListNode(1, ListNode(1)))))
+# 100100111000 000
+print(s.getDecimalValue(
+    ListNode(1, ListNode(0, ListNode(0,
+                                     ListNode(1, ListNode(0, ListNode(0,
+                                                                      ListNode(1, ListNode(1, ListNode(1,
+                                                                                                       ListNode(0,
+                                                                                                                ListNode(
+                                                                                                                    0,
+                                                                                                                    ListNode(
+                                                                                                                        0,
+                                                                                                                        ListNode(
+                                                                                                                            0,
+                                                                                                                            ListNode(
+                                                                                                                                0,
+                                                                                                                                ListNode(
+                                                                                                                                    0)))
+                                                                                                                    )))
+                                                                                                       )))))))))))
