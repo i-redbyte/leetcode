57b1c225519be4943c2b5bff5d98288545db0867
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-08 00:20:07 +0300

    @solution: Number of Good Pairs(second solution)

--- easy/number of good pairs/solution.py
+++ easy/number of good pairs/solution.py
@@ -1,12 +1,24 @@
+import collections
 from typing import List
 
 
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        d = {}
+        result = 0
+        for i in nums:
+            if i in d:
+                result += d[i]
+                d[i] += 1
+            else:
+                d[i] = 1
+        return result
+
+    def numIdenticalPairs1(self, nums: List[int]) -> int:
         result = 0
         n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1, n):
+        for i in range(n - 1):
+            for j in range(i + 1, n):
                 if nums[i] == nums[j]:
                     result += 1
         return result
