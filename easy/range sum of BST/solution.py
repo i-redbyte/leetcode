40d8568b786348d358d608bed1fc394e23da64c1
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-15 00:20:07 +0300

    @solution: Range Sum of BST

---
+++ easy/range sum of BST/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root:
+            return 0
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+
+s = Solution()
+tree = TreeNode(10, TreeNode(5, TreeNode(3), TreeNode(7)), TreeNode(15, None, TreeNode(18)))
+print(s.rangeSumBST(tree, 7, 15))
