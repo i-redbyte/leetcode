0175d64729a06bd5b15f81b4397902d5cdf65c93
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-22 20:45:50 +0300

    @solution: Reorder List

---
+++ medium/reorder list/solution.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return head
+        h = head
+        h_next = head.next
+        stack = []
+        while head:
+            stack.append(head)
+            head = head.next
+        s = stack.pop()
+        s_prev = None
+        while h != s_prev and s != h_next:
+            s_prev = stack.pop()
+            h.next = s
+            s.next = h_next
+            s_prev.next = None
+            h = h_next
+            s = s_prev
+            h_next = h.next
+
+
+s = Solution()
+s.reorderList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+s.reorderList(ListNode(1, ListNode(2, ListNode(3, ListNode(4)))))
