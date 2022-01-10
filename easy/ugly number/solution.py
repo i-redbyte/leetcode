b1569b06dd219495e7db9a8122f839bcaf9f6024
Ilya Sokolov <developer.sokolov@gmail.com>
2022-01-11 02:30:30 +0300

    @solution: Ugly Number

--- easy/ugly number/solution.py
+++ easy/ugly number/solution.py
@@ -4,10 +4,21 @@ class Solution:
             a = a / b
         return a
 
-    def isUgly(self, n: int) -> bool:
+    def isUgly2(self, n: int) -> bool:
         n = self.maxDiv(n, 2)
         n = self.maxDiv(n, 3)
         n = self.maxDiv(n, 5)
+        return n == 1
+
+    def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
+        while n % 2 == 0:
+            n /= 2
+        while n % 3 == 0:
+            n /= 3
+        while n % 5 == 0:
+            n /= 5
         return n == 1
 
 
@@ -18,3 +29,5 @@ print(s.isUgly(8))
 print(s.isUgly(1))
 print(s.isUgly(14))
 print(s.isUgly(21))
+print(s.isUgly(0))
+print(s.isUgly(1))
