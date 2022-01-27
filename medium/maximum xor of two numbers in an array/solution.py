ceea5cc792eb8fbc2140f20744bdea8a01fb9189
Ilya Sokolov <developer.sokolov@gmail.com>
2022-01-27 21:36:13 +0300

    @solution: Maximum XOR of Two Numbers in an Array

--- medium/maximum xor of two numbers in an array/solution.py
+++ medium/maximum xor of two numbers in an array/solution.py
@@ -3,6 +3,18 @@ from typing import List
 
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
+        bin_number_max_len = len(bin(max(nums))) - 2
+        result = 0
+        for prefix_len in range(1, bin_number_max_len + 1):
+            result <<= 1
+            current = result | 1
+            shift = bin_number_max_len - prefix_len
+            prefixes = {n >> shift for n in nums}
+            found_match = any(current ^ prefix in prefixes for prefix in prefixes)
+            result |= found_match
+        return result
+
+    def findMaximumXOR1(self, nums: List[int]) -> int:
         result = 0
         n = len(nums)
         for i in range(n):
