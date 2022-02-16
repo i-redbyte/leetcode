bce24eff9279906db0683cba13a886e65ccd1732
Ilya Sokolov <developer.sokolov@gmail.com>
2022-02-17 00:06:29 +0300

    @solution: Swapping Nodes in a Linked List

---
+++ medium/swapping nodes in a linked list/solution.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        rabbit = head
+        turtle = head
+        while k > 1:
+            rabbit = rabbit.next
+            k -= 1
+        node_a = rabbit
+        while rabbit.next:
+            rabbit = rabbit.next
+            turtle = turtle.next
+        turtle.val, node_a.val = node_a.val, turtle.val
+        return head
+
+
+s = Solution()
+
+l = s.swapNodes(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2)
+while l:
+    print(l.val)
+    l = l.next
