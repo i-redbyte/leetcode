6534c9f1b4656b4eb4f5a1b283ea2374a741400e
Ilya Sokolov <developer.sokolov@gmail.com>
2022-02-21 21:42:47 +0300

    @solution: Majority Element (solution #2)

--- easy/majority element/solution.py
+++ easy/majority element/solution.py
@@ -2,7 +2,20 @@ from typing import List
 
 
 class Solution:
+    # Boyer-Moore Voting Algorithm
     def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
+
+    def majorityElement1(self, nums: List[int]) -> int:
         n = len(nums)
         if len(nums) < 3:
             return max(nums)
@@ -14,3 +27,4 @@ class Solution:
 print(Solution().majorityElement([3, 2, 3]))
 print(Solution().majorityElement([3, 3, 4]))
 print(Solution().majorityElement([2, 2, 1, 1, 1, 2, 2]))
+print(Solution().majorityElement([6,5,5]))
