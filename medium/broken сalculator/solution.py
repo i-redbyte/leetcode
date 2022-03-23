cd2d5512402e69e1bb1b5f776c34f808bbf9759d
Ilya Sokolov <developer.sokolov@gmail.com>
2022-03-23 23:08:17 +0300

    @solution: Broken Calculator

---
+++ medium/broken сalculator/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        result = 0
+        while target > startValue:
+            result += 1
+            if target & 1 != 0:
+                target += 1
+            else:
+                target //= 2
+        return result + startValue - target
+
+
+print(Solution().brokenCalc(2, 3))
+print(Solution().brokenCalc(5, 8))
+print(Solution().brokenCalc(3, 10))
