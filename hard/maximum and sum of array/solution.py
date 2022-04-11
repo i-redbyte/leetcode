ba188fd3058d736668a2bde6bcb8dc4c974ad10f
Ilya Sokolov <developer.sokolov@gmail.com>
2022-04-12 00:14:33 +0300

    @solution: Maximum AND Sum of Array

--- hard/maximum and sum of array/solution.py
+++ hard/maximum and sum of array/solution.py
@@ -1,8 +1,30 @@
+from functools import lru_cache
 from typing import List
 
 
 class Solution:
     def maximumANDSum(self, nums: List[int], numSlots: int) -> int:
+        ans = 0
+        X = tuple([2] * numSlots)
+        for j in range(1, numSlots + 1):
+            if j in nums and X[j - 1] > 0:
+                X = X[:j - 1] + (X[j - 1] - 1,) + X[j:]
+                nums.remove(j)
+                ans += j
+        n = len(nums)
+
+        @lru_cache(None)
+        def dp(curr, i):
+            if i == n:
+                return 0
+            return max(
+                (nums[i] & (j + 1)) + dp(curr[:j] + (curr[j] - 1,) + curr[j + 1:], i + 1) for j in range(numSlots) if
+                curr[j] > 0)
+
+        return ans + dp(X, 0)
+
+    # Time Limit Exceeded
+    def maximumANDSum1(self, nums: List[int], numSlots: int) -> int:
         def dfs(nums, slots) -> int:
             if not nums:
                 return 0
