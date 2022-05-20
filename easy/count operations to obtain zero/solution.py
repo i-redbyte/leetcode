c18cea16ae9d7ba434701202ccdf2f33de17a619
Ilya Sokolov <developer.sokolov@gmail.com>
2022-05-20 13:36:05 +0300

    @solution: Count Operations to Obtain Zero (second solution)

--- easy/count operations to obtain zero/solution.py
+++ easy/count operations to obtain zero/solution.py
@@ -1,5 +1,10 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
+        if num1 * num2 == 0:
+            return 0
+        return num1 // num2 + self.countOperations(num2, num1 % num2)
+
+    def countOperations1(self, num1: int, num2: int) -> int:
         result = 0
         while num1 != 0 and num2 != 0:
             if num1 >= num2:
