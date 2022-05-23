3754513ef3322d809a0c1f92610eb7c7f2e75cc7
Ilya Sokolov <developer.sokolov@gmail.com>
2022-05-23 18:41:19 +0300

    @solution: Valid Perfect Square (4 solution)

--- easy/valid perfect square/solution.py
+++ easy/valid perfect square/solution.py
@@ -1,5 +1,18 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
+        left = 0
+        right = num
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid ** 2 == num:
+                return True
+            elif mid ** 2 > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
+
+    def isPerfectSquare3(self, num: int) -> bool:
         i = 1
         while num > 0:
             num -= i
