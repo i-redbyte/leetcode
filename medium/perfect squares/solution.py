45da9f20ac5e355658b69ae9c9050e92c59bcfa2
Ilya Sokolov <developer.sokolov@gmail.com>
2022-05-26 13:13:10 +0300

    @solution: Fraction to Recurring Decimal (faster than 99.31% solution)

--- medium/perfect squares/solution.py
+++ medium/perfect squares/solution.py
@@ -1,5 +1,19 @@
+from math import sqrt
+
+
 class Solution:
     def numSquares(self, n: int) -> int:
+        while n % 4 == 0:
+            n //= 4
+        if n % 8 == 7:
+            return 4
+        for a in range(int(sqrt(n)) + 1):
+            b = int(sqrt(n - a * a))
+            if (a ** 2 + b ** 2) == n:
+                return (a > 0) + (b > 0)
+        return 3
+
+    def numSquares1(self, n: int) -> int:  # Time Limit Exceeded
         dp = [float("inf")] * (n + 1)
         dp[0] = 0
         for i in range(0, n + 1):
