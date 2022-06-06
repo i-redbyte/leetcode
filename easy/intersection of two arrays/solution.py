3c3bfca7dd90dd86dce0a6c6201eb4366ce43859
Ilya Sokolov <developer.sokolov@gmail.com>
2022-06-07 01:08:05 +0300

    @solution: Intersection of Two Arrays (solution 4)

--- easy/intersection of two arrays/solution.py
+++ easy/intersection of two arrays/solution.py
@@ -3,6 +3,13 @@ from typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        for i in nums1:
+            if i not in result and i in nums2:
+                result.append(i)
+        return result
+
+    def intersection3(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2))
 
     def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
