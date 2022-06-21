d7c4a082e26a7278eba234ee0c9be2e8b708b5ef
Ilya Sokolov <developer.sokolov@gmail.com>
2022-06-21 21:35:22 +0300

    @solution: XOR Operation in an Array (2 version)

--- easy/xor operation in an array/solution.py
+++ easy/xor operation in an array/solution.py
@@ -1,5 +1,15 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
+        last = start + 2 * (n - 1)
+        if start % 4 < 2:
+            start = 0
+        else:
+            n -= 1
+        if n % 2 == 0:
+            return start ^ (n & 2)
+        return start ^ last ^ (n & 2)
+
+    def xorOperation1(self, n: int, start: int) -> int:
         result = 0
         for i in range(n):
             result ^= start + 2 * i
