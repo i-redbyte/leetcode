e28ba2f9f01c1277cdee9d92d88ccfeba4b7b652
Ilya Sokolov <developer.sokolov@gmail.com>
2022-06-27 23:05:11 +0300

    @solution: Partitioning Into Minimum Number Of Deci-Binary Numbers (two version)

--- medium/partitioning into minimum number of deci-binary numbers/solution.py
+++ medium/partitioning into minimum number of deci-binary numbers/solution.py
@@ -1,5 +1,12 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
+        nums = set(n)
+        for i in range(9, 0, -1):
+            if str(i) in nums:
+                return i
+        return 0
+
+    def minPartitions1(self, n: str) -> int:
         result = "0"
         for ch in n:
             result = max(ch, result)
