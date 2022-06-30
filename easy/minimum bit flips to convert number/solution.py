76a2e19f0a6f074244ab488b792948748e768028
Ilya Sokolov <developer.sokolov@gmail.com>
2022-06-30 12:19:10 +0300

    @solution: Minimum Bit Flips to Convert Number

---
+++ easy/minimum bit flips to convert number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        condition = start ^ goal
+        result = 0
+        while condition > 0:
+            condition = condition & (condition - 1)
+            result += 1
+        return result
+
+
+print(Solution().minBitFlips(start=10, goal=7))
+print(Solution().minBitFlips(3, 4))
