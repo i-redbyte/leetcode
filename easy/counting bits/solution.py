dd82d09f3479a85bc87668fd0f2c2e4195e4fd54
Ilya Sokolov <developer.sokolov@gmail.com>
2022-07-14 12:03:00 +0300

    @solution: Counting Bits (two version O(n))

--- easy/counting bits/solution.py
+++ easy/counting bits/solution.py
@@ -3,6 +3,13 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        l = n + 1
+        result = [0] * l
+        for i in range(1, l):
+            result[i] = result[i >> 1] + (i & 1)
+        return result
+
+    def countBits1(self, n: int) -> List[int]:
         result = []
         for i in range(n + 1):
             counter = 0
@@ -17,3 +24,4 @@ class Solution:
 s = Solution()
 print(s.countBits(2))
 print(s.countBits(5))
+print(s.countBits(777))
