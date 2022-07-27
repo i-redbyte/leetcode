bc88f30cd04d05341688f37915867333e364cd85
Ilya Sokolov <developer.sokolov@gmail.com>
2022-07-27 23:06:10 +0300

    @solution: Build Array from Permutation (binary solution)

--- easy/build array from permutation/solution.py
+++ easy/build array from permutation/solution.py
@@ -3,9 +3,19 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        zero = None
+        for i, v in enumerate(nums):
+            if v == 0:
+                zero = i
+            nums[i] = nums[i] << 16 | (t if (t := nums[nums[i]] >> 16) > 0 or nums[i] == zero else nums[nums[i]])
+        for i in range(len(nums)):
+            nums[i] = nums[i] & (1 << 16) - 1
+        return nums
+
+    def buildArray3(self, nums: List[int]) -> List[int]:
         return [nums[i] for i in nums]
 
-    def buildArray(self, nums: List[int]) -> List[int]:
+    def buildArray2(self, nums: List[int]) -> List[int]:
         n = len(nums)
         for i, v in enumerate(nums):
             nums[i] += n * (nums[v] % n)
