8dc84e9b26b67a81dcf48990a9a3cfdd9708a31e
Ilya Sokolov <developer.sokolov@gmail.com>
2022-07-28 02:52:34 +0300

    @solution: Max Consecutive Ones

---
+++ easy/max consecutive ones/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        result = 0
+        counter = 0
+        for i in nums:
+            if i == 1:
+                counter += 1
+                result = max(counter, result)
+            else:
+                counter = 0
+        return result
+
+
+s = Solution()
+print(s.findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
+print(s.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]))
+print(s.findMaxConsecutiveOnes([0, 0]))
+print(s.findMaxConsecutiveOnes([0]))
+print(s.findMaxConsecutiveOnes([1]))
+print(s.findMaxConsecutiveOnes([]))
