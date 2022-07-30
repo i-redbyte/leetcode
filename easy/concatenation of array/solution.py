1ec88d0549c331f08d2b880ae4f1066e6962de92
Ilya Sokolov <developer.sokolov@gmail.com>
2022-07-30 21:39:29 +0300

    @solution: Concatenation of Array (first solution)

--- easy/concatenation of array/solution.py
+++ easy/concatenation of array/solution.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        m = 2 * n
+        result = [0] * m
+        for i, v in enumerate(nums):
+            result[i] = v
+            result[n + i] = v
+        return result
+
+    def getConcatenation1(self, nums: List[int]) -> List[int]:
         return nums + nums
 
 
