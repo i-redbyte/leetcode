34822f88d684386cf85751c3a15ecbb91dae9b7a
Ilya Sokolov <developer.sokolov@gmail.com>
2022-07-31 16:48:24 +0300

    @solution: Minimum Time Difference

---
+++ medium/minimum time difference/README.md
@@ -0,0 +1,23 @@
+# Minimum Time Difference
+
+Given a list of 24-hour clock time points in "HH:MM" format, return the minimum minutes difference between any two time-points in the list.
+ 
+Example 1:
+
+```
+Input: timePoints = ["23:59","00:00"]
+Output: 1
+```
+
+Example 2:
+
+```
+Input: timePoints = ["00:00","23:59","00:00"]
+Output: 0
+```
+
+**Constraints:**
+- 2 <= timePoints.length <= 2 * 104
+- timePoints[i] is in the format "HH:MM".
+
+[Link](https://leetcode.com/problems/minimum-time-difference/)
---
+++ medium/minimum time difference/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def getMinutes(self, hh: str, mm: str) -> int:
+        h = int(hh)
+        m = int(mm)
+        return (h * 60) + m
+
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        if len(timePoints) < 2:
+            return 0
+        minutes = []
+        for t in timePoints:
+            item = t.split(":")
+            minutes.append(self.getMinutes(item[0], item[1]))
+        minutes.sort()
+        minutes.append(minutes[0] + 1440)
+        n = len(minutes)
+        result = 99999999
+        for i in range(1, n):
+            result = min(result, minutes[i] - minutes[i - 1])
+        return result
+
+
+s = Solution()
+print(s.findMinDifference(["23:59", "00:00"]))
+print(s.findMinDifference(["00:00", "23:59", "00:00"]))
+print(s.findMinDifference(["12:12", "00:13"]))
+print(s.findMinDifference(["01:01", "02:01", "03:00"]))
