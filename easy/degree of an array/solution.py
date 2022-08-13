835b90a6b1f9d8a952c88f0f3e28483a8f4664d2
Ilya Sokolov <developer.sokolov@gmail.com>
2022-08-13 22:58:53 +0300

    @solution: Degree of an Array (second solution)

--- easy/degree of an array/solution.py
+++ easy/degree of an array/solution.py
@@ -3,7 +3,25 @@ from typing import List
 
 
 class Solution:
-    def findShortestSubArray(self, nums: List[int]) -> int:
+    def findShortestSubArray(self, nums):
+        dick = {}
+        tmp = 1
+        result = 1
+        for i, x in enumerate(nums):
+            if x in dick:
+                dick[x][1] += 1
+                m = dick[x][1]
+                length = i - dick[x][0] + 1
+            else:
+                dick[x] = [i, 1]
+                length = m = 1
+            if m > tmp:
+                tmp, result = m, length
+            elif tmp == m:
+                result = min(result, length)
+        return result
+
+    def findShortestSubArray1(self, nums: List[int]) -> int:
         d = defaultdict(list)
         for i, x in enumerate(nums):
             d[x].append(i)
