e6ea31f42c38e7ef5711937e95dd54f92a58138e
Ilya Sokolov <developer.sokolov@gmail.com>
2022-08-16 19:00:56 +0300

    @solution: First Letter to Appear Twice (second solution)

--- easy/first letter to appear twice/solution.py
+++ easy/first letter to appear twice/solution.py
@@ -1,5 +1,13 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
+        alphabet = [0] * 26
+        for c in s:
+            alphabet[ord(c) - ord('a')] += 1
+            if alphabet[ord(c) - ord('a')] == 2:
+                return c
+        return ""
+
+    def repeatedCharacter1(self, s: str) -> str:
         tmp = set()
         for (i, c) in enumerate(s):
             if c in tmp:
