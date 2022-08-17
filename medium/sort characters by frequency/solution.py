6ce101d8446c892cf7c366d110979c586d762a5f
Ilya Sokolov <developer.sokolov@gmail.com>
2022-08-18 01:34:00 +0300

    @solution: Sort Characters By Frequency (2 version)

--- medium/sort characters by frequency/solution.py
+++ medium/sort characters by frequency/solution.py
@@ -3,6 +3,13 @@ import collections
 
 class Solution:
     def frequencySort(self, s: str) -> str:
+        result = ''
+        l = sorted(list(set(s)), key=s.count, reverse=True)
+        for i in l:
+            result += i * s.count(i)
+        return result
+
+    def frequencySort1(self, s: str) -> str:
         return "".join(char * times for char, times in collections.Counter(s).most_common())
 
 
