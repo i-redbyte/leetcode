7b65096ededf47dd7c2b43f16532f0819f95894b
Ilya Sokolov <developer.sokolov@gmail.com>
2022-08-26 01:34:50 +0300

    @solution: Find XOR Sum of All Pairs Bitwise AND

---
+++ hard/find xor sum of all pairs bitwise and/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def getXORSum(self, arr1: List[int], arr2: List[int]) -> int:
+        a = arr1[0]
+        b = arr2[0]
+        n = len(arr1)
+        m = len(arr2)
+        for i in range(1, n):
+            a = a ^ arr1[i]
+        for j in range(1, m):
+            b = b ^ arr2[j]
+        return a & b
+
+
+print(Solution().getXORSum(arr1=[1, 2, 3], arr2=[6, 5]))
+print(Solution().getXORSum(arr1=[12], arr2=[4]))
