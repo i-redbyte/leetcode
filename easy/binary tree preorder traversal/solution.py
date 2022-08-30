d8c69b960a280785d1e6860640fd798de05517fc
Ilya Sokolov <developer.sokolov@gmail.com>
2022-08-30 12:20:41 +0300

    @solution: Binary Tree Preorder Traversal(2 version)

--- easy/binary tree preorder traversal/solution.py
+++ easy/binary tree preorder traversal/solution.py
@@ -11,6 +11,20 @@ class TreeNode:
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def preorder(node):
+            if node:
+                result.append(node.val)
+                preorder(node.left)
+                preorder(node.right)
+
+        if not root:
+            return result
+        preorder(root)
+        return result
+
+    def preorderTraversal1(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
