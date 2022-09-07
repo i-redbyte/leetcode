f69f9b031ca517305fb42775d0d07fa628725b3a
Ilya Sokolov <developer.sokolov@gmail.com>
2022-09-07 18:21:30 +0300

    @solution: Construct String from Binary Tree

---
+++ easy/construct string from binary tree/solution.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+        if root.left is None and root.right is None:
+            return str(root.val)
+        if root.right is None:
+            return "{val}({left})".format(val=root.val, left=self.tree2str(root.left))
+        return "{val}({left})({right})".format(val=root.val, left=self.tree2str(root.left),
+                                               right=self.tree2str(root.right))
+
+
+s = Solution()
+t = TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3))
+print(s.tree2str(t))
