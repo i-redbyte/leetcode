91b022e73dc4b12eae6426a5b974a42a36ec159b
Ilya Sokolov <developer.sokolov@gmail.com>
2022-09-09 00:25:00 +0300

    @solution: Unique Morse Code Words

--- easy/unique morse code words/solution.py
+++ easy/unique morse code words/solution.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
@@ -8,10 +9,23 @@ class Solution:
                  "---", ".--.", "--.-", ".-.", "...", "-", "..-",
                  "...-", ".--", "-..-", "-.--", "--.."]
 
-        seen = {"".join(morse[ord(c) - ord('a')] for c in word)
-                for word in words}
+        s = set("")
+        for w in words:
+            code = []
+            for ch in w:
+                code.append(morse[ord(ch) - ord('a')])
+            s.add(''.join(code))
+        return len(s)
 
-        return len(seen)
+    def uniqueMorseRepresentations1(self, words: List[str]) -> int:
+        morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                 "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                 "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                 "...-", ".--", "-..-", "-.--", "--.."]
+
+        result = {"".join(morse[ord(c) - ord('a')] for c in word)
+                for word in words}
+        return len(result)
 
 
 s = Solution()
