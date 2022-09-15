79d535a0253b8f57dbf22c2a1efa71148491fa07
Ilya Sokolov <developer.sokolov@gmail.com>
2022-09-16 00:48:54 +0300

    @solution: Binary Tree Paths

---
+++ easy/binary tree paths/solution.py
@@ -0,0 +1,33 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        def computed(root: Optional[TreeNode], buffer) -> List[str]:
+            if not root.left and not root.right:
+                return [buffer + f"{root.val}"]
+
+            left = right = list()
+
+            if root.left:
+                l_path = buffer + f"{root.val}" + "->"
+                left = computed(root.left, l_path)
+
+            if root.right:
+                r_path = buffer + f"{root.val}" + "->"
+                right = computed(root.right, r_path)
+            return left + right
+
+        return computed(root, "")
+
+
+s = Solution()
+t = TreeNode(1, TreeNode(2, None, TreeNode(5)), TreeNode(3))
+print(s.binaryTreePaths(t))
