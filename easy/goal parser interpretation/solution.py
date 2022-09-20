3eb1e0877ccd6986e23c303e943f24d2c1727b4b
Ilya Sokolov <developer.sokolov@gmail.com>
2022-09-20 17:08:07 +0300

    @solution: Goal Parser Interpretation(norm solution)

--- easy/goal parser interpretation/solution.py
+++ easy/goal parser interpretation/solution.py
@@ -1,6 +1,23 @@
 class Solution:
-    def interpret(self, command: str) -> str:
+    def interpret1(self, command: str) -> str:
         return command.replace('()', 'o').replace('(al)', 'al')
+
+    def interpret(self, command: str) -> str:
+        result = ''
+        n = len(command)
+        i = 0
+        while i < n:
+            if command[i] == 'G':
+                result += 'G'
+            if command[i] == '(':
+                if command[i + 1] == ')':
+                    result += 'o'
+                    i += 1
+                else:
+                    result += 'al'
+                    i += 3
+            i += 1
+        return result
 
 
 s = Solution()
