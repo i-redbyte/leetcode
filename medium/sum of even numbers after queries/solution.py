f4373bfc0a7b35b185a06a945eecbdd9e3af5dc2
Ilya Sokolov <developer.sokolov@gmail.com>
2022-09-21 16:38:36 +0300

    @solution: Sum of Even Numbers After Queries (solution 2)

--- medium/sum of even numbers after queries/solution.py
+++ medium/sum of even numbers after queries/solution.py
@@ -3,6 +3,21 @@ from typing import List
 
 class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        result = []
+        evenSum = 0
+        for _ in nums:
+            if not _ & 1:
+                evenSum += _
+        for val, i in queries:
+            num = nums[i]
+            evenSum = evenSum - num if not num & 1 else evenSum
+            num += val
+            nums[i] = num
+            evenSum = evenSum + num if not num & 1 else evenSum
+            result.append(evenSum)
+        return result
+
+    def sumEvenAfterQueries1(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         s = sum(x for x in nums if x % 2 == 0)
         result = []
         for x, i in queries:
