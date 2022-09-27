69f861115a10d749cd10d6ddb213fb9635e8b054
Ilya Sokolov <developer.sokolov@gmail.com>
2022-09-27 22:13:39 +0300

    @solution: Number Complement (version 3)

--- easy/number complement/solution.py
+++ easy/number complement/solution.py
@@ -1,6 +1,8 @@
-
 class Solution:
     def findComplement(self, num: int) -> int:
+        return (1 << len(bin(num)) - 2) - 1 - num
+
+    def findComplement2(self, num: int) -> int:
         return num ^ (2 ** num.bit_length() - 1)
 
     def findComplement1(self, num: int) -> int:
