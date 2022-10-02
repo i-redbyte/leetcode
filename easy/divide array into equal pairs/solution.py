145c4307275ed56f8fe529c53c79472b2a187a45
Ilya Sokolov <developer.sokolov@gmail.com>
2022-10-03 01:45:49 +0300

    @solution: Divide Array Into Equal Pairs (fast solution)

---
+++ easy/divide array into equal pairs/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        dick = {}
+        for n in nums:
+            if n in dick:
+                dick[n] += 1
+            else:
+                dick[n] = 1
+        for v in dick.values():
+            if v & 1 != 0:
+                return False
+        return True
+
+
+s = Solution()
+print(s.divideArray([3, 2, 3, 2, 2, 2]))
+print(s.divideArray([1, 2, 3, 4]))
+print(s.divideArray([9, 4, 18, 3, 2, 6, 18, 15, 7, 15, 6, 4, 15, 14, 7, 4, 15, 4, 3, 17, 9, 13, 13, 12, 2, 14, 12, 17]))
