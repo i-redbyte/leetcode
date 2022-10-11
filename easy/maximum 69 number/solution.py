2a1f76164f37c9da424810e064c4c3663dc341f1
Ilya Sokolov <developer.sokolov@gmail.com>
2022-10-11 22:24:06 +0300

    @solution: Maximum 69 Number (second solution)

--- easy/maximum 69 number/solution.py
+++ easy/maximum 69 number/solution.py
@@ -1,5 +1,20 @@
 class Solution:
     def maximum69Number(self, num: int) -> int:
+        current = 0
+        firstSixIndex = -1
+        copyValue = num
+        while copyValue:
+            if copyValue % 10 == 6:
+                firstSixIndex = current
+            copyValue //= 10
+            current += 1
+
+        if firstSixIndex == -1:
+            return num
+        else:
+            return num + 3 * (10 ** firstSixIndex)
+
+    def maximum69Number1(self, num: int) -> int:
         s = str(num)
         n = len(s)
         for i in range(n):
