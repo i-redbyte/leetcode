060f85c0484d17026c841c318c742bb462c1be25
Ilya Sokolov <developer.sokolov@gmail.com>
2022-10-13 20:19:17 +0300

    @solution: Complement of Base 10 Integer(solution)

--- easy/complement of base 10 integer/solution.py
+++ easy/complement of base 10 integer/solution.py
@@ -1,8 +1,17 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
+        if n == 0:
+            return 1
+        mask = ~0
+        while n & mask:
+            mask <<= 1
+        return ~mask & ~n
+
+    def bitwiseComplement1(self, n: int) -> int:
         return (1 << len(bin(n)) - 2) - 1 - n
 
 
 print(Solution().bitwiseComplement(5))
 print(Solution().bitwiseComplement(7))
 print(Solution().bitwiseComplement(10))
+print(Solution().bitwiseComplement(0))
