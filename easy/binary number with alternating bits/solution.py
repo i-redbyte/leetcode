235b486ba460f9d4bf8ba1561f20230504d094b4
Ilya Sokolov <developer.sokolov@gmail.com>
2022-10-19 17:16:40 +0300

    @solution: Binary Number with Alternating Bits

---
+++ easy/binary number with alternating bits/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        b = n & 1
+        while n > 0:
+            if n & 1 != b:
+                return False
+            n >>= 1
+            b = 1 - b
+        return True
+
+
+s = Solution()
+print(s.hasAlternatingBits(4))
+print(s.hasAlternatingBits(5))
+print(s.hasAlternatingBits(7))
+print(s.hasAlternatingBits(11))
+print(s.hasAlternatingBits(10))
+print(s.hasAlternatingBits(100))
+print(s.hasAlternatingBits(42))
