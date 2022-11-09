5cf4bbc8d0cd5a3029c88d0eea6f7c38a2f2a233
Ilya Sokolov <developer.sokolov@gmail.com>
2022-11-10 01:33:45 +0300

    @solution: Check if the Sentence Is Pangram (second solution)

--- easy/check if the sentence Is pangram/solution.py
+++ easy/check if the sentence Is pangram/solution.py
@@ -1,5 +1,11 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        result = {}
+        for s in sentence:
+            result[s] = 1
+        return len(result.keys()) == 26
+
+    def checkIfPangram1(self, sentence: str) -> bool:
         alphabet = set("qwertyuioplkjhgfdsazxcvbnm")
         s = set(sentence)
         return len(alphabet.difference(s)) == 0
