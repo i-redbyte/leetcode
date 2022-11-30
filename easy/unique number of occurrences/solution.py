7514cf5301f98fe619abae2fbee9a3b6e34198f4
Ilya Sokolov <developer.sokolov@gmail.com>
2022-11-30 21:13:35 +0300

    @solution: Unique Number of Occurrences (second solution)

--- easy/unique number of occurrences/solution.py
+++ easy/unique number of occurrences/solution.py
@@ -5,6 +5,15 @@ from typing import List
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         c = collections.Counter(arr)
+        s = sorted(c.values())
+        n = len(s)
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                return False
+        return True
+
+    def uniqueOccurrences1(self, arr: List[int]) -> bool:
+        c = collections.Counter(arr)
         n = len(c.values())
         m = len(set(c.values()))
         return n == m
