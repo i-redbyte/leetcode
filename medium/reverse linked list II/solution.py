0300bda1d6b9c76de088619c09f168f528a3cc1e
Ilya Sokolov <developer.sokolov@gmail.com>
2022-12-15 12:26:17 +0300

    @solution: Reverse Linked List II

---
+++ medium/reverse linked list II/solution.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+        tmp = ListNode(0)
+        tmp.next = head
+        prev = tmp
+
+        n = left - 1
+        for i in range(n):
+            prev = prev.next
+
+        reverse = None
+        current = prev.next
+        m = right - left + 1
+        for i in range(m):
+            nxt = current.next
+            current.next = reverse
+            reverse = current
+            current = nxt
+
+        prev.next.next = current
+        prev.next = reverse
+        return tmp.next
+
+
+s = Solution()
+list = s.reverseBetween(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2, 4)
+
+while list:
+    print(list.val)
+    list = list.next
