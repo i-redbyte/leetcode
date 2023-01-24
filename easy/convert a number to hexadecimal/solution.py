d087e5b834c0690375ef04451a7cef4d15ca7d5f
Ilya Sokolov <developer.sokolov@gmail.com>
2023-01-24 23:42:58 +0300

    @solution: Convert a Number to Hexadecimal

---
+++ easy/convert a number to hexadecimal/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if 0 <= num < 10:
+            return str(num)
+        result = ""
+        power = {10: "a", 11: "b", 12: "c", 13: "d", 14: "e", 15: "f"}
+        n = num
+        if num < 0:
+            n = 4294967296 + num  # n = 2 ** 32 + num
+        while n > 0:
+            m = n % 16
+            if m > 9:
+                result = power[m] + result
+            else:
+                result = str(m) + result
+            n = n // 16
+        return result
+
+
+s = Solution()
+print(s.toHex(26))
+print(s.toHex(-1))
+print(s.toHex(0))
+print(s.toHex(7))
