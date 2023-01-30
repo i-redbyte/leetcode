4b7b378a0084aba4df155156c4737be63bfcf6fa
Ilya Sokolov <developer.sokolov@gmail.com>
2023-01-30 14:12:53 +0300

    @solution: N-th Tribonacci Number (3-th solution)

--- easy/n-th tribonacci number/solution.py
+++ easy/n-th tribonacci number/solution.py
@@ -1,5 +1,19 @@
 class Solution:
+    mem = [0] * 38
+
     def tribonacci(self, n: int) -> int:
+        if self.mem[n] != 0:
+            return self.mem[n]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 1
+        self.mem[n] = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+        return self.mem[n]
+
+    def tribonacci2(self, n: int) -> int:
         if n == 0:
             return 0
         if n == 1:
