fa9af67e45046d9ecc947d9f58eb14fe56a29f87
Ilya Sokolov <developer.sokolov@gmail.com>
2023-02-09 17:08:49 +0300

    @solution_bash: Jump Game add bad recursion solution

--- medium/jump game/solution.py
+++ medium/jump game/solution.py
@@ -2,7 +2,21 @@ from typing import List
 
 
 class Solution:
+    def solution(self, nums: List[int], x: int) -> bool:
+        n = len(nums) - 1
+        if x == n:
+            return True
+        if x >= n+1 or nums[x] == 0:
+            return False
+        result = False
+        for i in range(1, nums[x]+1):
+            result = result or self.solution(nums, x + i)
+        return result
+
     def canJump(self, nums: List[int]) -> bool:
+        return self.solution(nums, 0)
+
+    def canJump1(self, nums: List[int]) -> bool:
         n = len(nums) - 1
         j = n
         for i in range(n, -1, -1):
