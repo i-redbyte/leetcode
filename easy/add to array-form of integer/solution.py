799bc4dccae0d6f5a7d33dbcfc4c47356c7b7aac
Ilya Sokolov <developer.sokolov@gmail.com>
2023-02-15 22:44:05 +0300

    @solution: Add to Array-Form of Integer (the best solution)

--- easy/add to array-form of integer/solution.py
+++ easy/add to array-form of integer/solution.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        n = len(num) - 1
+        for i in range(n, -1, -1):
+            k, num[i] = divmod(num[i] + k, 10)
+        while k != 0:
+            k, a = divmod(k, 10)
+            num = [a] + num
+        return num
+
+    def addToArrayForm1(self, num: List[int], k: int) -> List[int]:
         s = 0
         n = len(num) - 1
         for i, v in enumerate(num):
@@ -20,3 +29,4 @@ print(s.addToArrayForm(num=[1], k=34))
 print(s.addToArrayForm(num=[1, 2, 0, 0], k=34))
 print(s.addToArrayForm(num=[2, 7, 4], k=181))
 print(s.addToArrayForm(num=[2, 1, 5], k=806))
+print(s.addToArrayForm(num=[2, 1, 5], k=999806))
