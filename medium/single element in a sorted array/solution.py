3dc6ba8ff7855cfc7526f95a9f6eb93c70eb3d2f
Ilya Sokolov <developer.sokolov@gmail.com>
2023-02-21 23:44:31 +0300

    @solution: Single Element in a Sorted Array (solution O(log n))

--- medium/single element in a sorted array/solution.py
+++ medium/single element in a sorted array/solution.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] != nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 2
+        return nums[left]
+
+    def singleNonDuplicate1(self, nums: List[int]) -> int:
         result = nums[0]
         n = len(nums)
         for i in range(1, n):
