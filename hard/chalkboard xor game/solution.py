821aabc109cdf0cff946a26802f8cd5f5adfb27a
Ilya Sokolov <developer.sokolov@gmail.com>
2023-02-28 20:21:39 +0300

    @solution: Chalkboard XOR Game (functional-style solution)

---
+++ hard/chalkboard xor game/solution.py
@@ -0,0 +1,13 @@
+from functools import reduce
+from typing import List
+
+
+class Solution:
+    def xorGame(self, nums: List[int]) -> bool:
+        return reduce(lambda a, b: a ^ b, nums) == 0 or len(nums) & 1 == 0
+
+
+s = Solution()
+print(s.xorGame([1, 1, 2]))
+print(s.xorGame([0, 1]))
+print(s.xorGame([1, 2, 3]))
