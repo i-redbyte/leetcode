823d2e00a7232e6847f71bee8fa3cc9c09d7bd94
Ilya Sokolov <developer.sokolov@gmail.com>
2023-02-28 20:24:09 +0300

    @solution: Chalkboard XOR Game (simple solution)

--- hard/chalkboard xor game/solution.py
+++ hard/chalkboard xor game/solution.py
@@ -4,6 +4,12 @@ from typing import List
 
 class Solution:
     def xorGame(self, nums: List[int]) -> bool:
+        result = 0
+        for i in nums:
+            result = result ^ i
+        return result == 0 or len(nums) & 1 == 0
+
+    def xorGame1(self, nums: List[int]) -> bool:
         return reduce(lambda a, b: a ^ b, nums) == 0 or len(nums) & 1 == 0
 
 
