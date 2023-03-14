93ebf1f7ff63eaabbd3f063bcb4ca50b9275e7aa
Ilya Sokolov <developer.sokolov@gmail.com>
2023-03-14 14:08:15 +0300

    @fix: Valid Palindrome II (recursion solution)

--- easy/valid palindrome II/solution.py
+++ easy/valid palindrome II/solution.py
@@ -1,5 +1,22 @@
 class Solution:
+
     def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(left: int, right: int, changed: bool) -> bool:
+            while left < right:
+                if s[left] != s[right]:
+                    if not changed:
+                        return isPalindrome(left + 1, right, True) or isPalindrome(left, right - 1, True)
+                    else:
+                        return False
+                else:
+                    left += 1
+                    right -= 1
+            return True
+
+        n = len(s) - 1
+        return isPalindrome(0, n, False)
+
+    def validPalindrome1(self, s: str) -> bool:
         n = len(s)
         lo = 0
         hi = n - 1
