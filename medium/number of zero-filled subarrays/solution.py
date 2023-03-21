c2cd2c2efd7ca1c0a7e9423bccd93aa496ca6e95
Ilya Sokolov <developer.sokolov@gmail.com>
2023-03-21 14:27:29 +0300

    @solution:  Number of Zero-Filled Subarrays (Sliding Window version)

--- medium/number of zero-filled subarrays/solution.py
+++ medium/number of zero-filled subarrays/solution.py
@@ -4,6 +4,20 @@ from typing import List
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         result = 0
+        start = 0
+        end = 0
+        n = len(nums)
+        while end != n:
+            if nums[end] != 0:
+                end += 1
+                start = end
+            else:
+                result += end - start + 1
+                end += 1
+        return result
+
+    def zeroFilledSubarray1(self, nums: List[int]) -> int:
+        result = 0
         sub_array = 0
         for i in nums:
             if i == 0:
