a72cd98d3552f90d7f813eb54c307a2c8f66e974
Ilya Sokolov <developer.sokolov@gmail.com>
2023-03-27 18:54:03 +0300

    @solution: Minimum Path Sum (recursion solution - Time Limit Exceeded)

--- medium/minimum path sum/solution.py
+++ medium/minimum path sum/solution.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        def solution(i: int, j: int, grid: List[List[int]]) -> int:
+            if i == 0 and j == 0:
+                return grid[0][0]
+            if i < 0 or j < 0:
+                return 999999999999999999999999
+            vertical = grid[i][j] + solution(i - 1, j, grid)
+            horizontal = grid[i][j] + solution(i, j - 1, grid)
+            return min(horizontal, vertical)
+
+        n, m = len(grid), len(grid[0])
+        return solution(n - 1, m - 1, grid)
+
+    def minPathSum1(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
         for i in range(1, n):
             grid[i][0] += grid[i - 1][0]
