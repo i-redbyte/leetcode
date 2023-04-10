c0d8f0fd8aa0970d5e9a343179da64cd4a175a31
Ilya Sokolov <developer.sokolov@gmail.com>
2023-04-10 20:30:15 +0300

    @solution: Wildcard Matching (second solution)

--- hard/wildcard matching/solution.py
+++ hard/wildcard matching/solution.py
@@ -1,5 +1,30 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        i = 0
+        j = 0
+        last = 0
+        star = -1
+        n = len(p)
+        m = len(s)
+        while i < m:
+            if j < n and (s[i] == p[j] or p[j] == '?'):
+                i += 1
+                j += 1
+            elif j < n and p[j] == '*':
+                last = i
+                star = j
+                j += 1
+            elif star != -1:
+                j = star + 1
+                i = last + 1
+                last += 1
+            else:
+                return False
+        while j < n and p[j] == '*':
+            j += 1
+        return j == n
+
+    def isMatch1(self, s: str, p: str) -> bool:
         n = len(s)
         m = len(p)
 
