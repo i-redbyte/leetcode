30865805c89b7508ed82fc8f92aa38b5c19fc27b
Ilya Sokolov <developer.sokolov@gmail.com>
2023-04-13 14:07:49 +0300

    @solution: Remove Outermost Parentheses (without stack solution)

--- easy/remove outermost parentheses/solution.py
+++ easy/remove outermost parentheses/solution.py
@@ -1,6 +1,20 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
         result = ""
+        counter = 0
+        for i, ch in enumerate(s):
+            if ch == '(':
+                counter += 1
+            if ch == ')':
+                counter -= 1
+            if counter == 0:
+                result = s[1:i]
+                result += self.removeOuterParentheses(s[i + 1:])
+                break
+        return result
+
+    def removeOuterParentheses1(self, s: str) -> str:
+        result = ""
         stack = []
         for i, ch in enumerate(s):
             if ch == '(':
