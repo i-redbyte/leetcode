0ff59186d96581a65e567d3200a9783aab2cd9a2
Ilya Sokolov <developer.sokolov@gmail.com>
2023-04-18 17:12:09 +0300

    @solution: Delete Greatest Value in Each Row (recursion solution)

--- easy/delete greatest value in each row/solution.py
+++ easy/delete greatest value in each row/solution.py
@@ -3,6 +3,22 @@ from typing import List
 
 class Solution:
     def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        def solution(grid: List[List[int]], result: int) -> int:
+            if not grid[0]:
+                return result
+            current_max = 0
+            for line in grid:
+                max_value = max(line)
+                if max_value >= current_max:
+                    current_max = max_value
+                line.remove(max_value)
+
+            result += current_max
+            return solution(grid, result)
+
+        return solution(grid, 0)
+
+    def deleteGreatestValue1(self, grid: List[List[int]]) -> int:
         result = 0
         n = len(grid)
         m = len(grid[0])
