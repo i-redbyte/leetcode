fd63cd1d0e2a294f68c516d298b2fcf7bccba40b
Ilya Sokolov <developer.sokolov@gmail.com>
2023-04-25 21:55:10 +0300

    @solution: Merge Strings Alternately (two points solution)

--- easy/merge strings alternately/solution.py
+++ easy/merge strings alternately/solution.py
@@ -1,5 +1,22 @@
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+    def mergeAlternately(self, word1, word2):
+        m = len(word1)
+        n = len(word2)
+        i = 0
+        j = 0
+        result = ""
+
+        while i < m or j < n:
+            if i < m:
+                result += word1[i]
+                i += 1
+            if j < n:
+                result += word2[j]
+                j += 1
+
+        return result
+
+    def mergeAlternately1(self, word1: str, word2: str) -> str:
         min_s = word1
         max_s = word2
         if len(min_s) > len(max_s):
