603242d8692aab7de587cb00a64d0d5e632c1180
Ilya Sokolov <developer.sokolov@gmail.com>
2023-05-02 23:32:58 +0300

    @solution:  Sign of the Product of an Array (solution 3)

--- easy/sign of the product of an array/solution.py
+++ easy/sign of the product of an array/solution.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        neg = 1
+        for i in nums:
+            if i == 0:
+                return 0
+            if i < 0:
+                neg = -1 * neg
+        return neg
+
+    def arraySign2(self, nums: List[int]) -> int:
         neg = 0
         for i in nums:
             if i == 0:
