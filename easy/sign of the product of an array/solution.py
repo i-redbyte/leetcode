93e51c6cda40aec60ec17861db62b2a87319d95c
Ilya Sokolov <developer.sokolov@gmail.com>
2023-05-02 23:27:35 +0300

    @solution:  Sign of the Product of an Array (counting solution)

--- easy/sign of the product of an array/solution.py
+++ easy/sign of the product of an array/solution.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        neg = 0
+        for i in nums:
+            if i == 0:
+                return 0
+            if i < 0:
+                neg += 1
+        return -1 if (neg % 2 != 0) else 1
+
+    def arraySign1(self, nums: List[int]) -> int:
         mul = 1
         for i in nums:
             mul *= i
