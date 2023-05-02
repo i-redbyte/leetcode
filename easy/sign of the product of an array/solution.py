39704b4369048da71938e544337c497c414f1a32
Ilya Sokolov <developer.sokolov@gmail.com>
2023-05-02 23:37:51 +0300

    @solution:  Sign of the Product of an Array (bit manipulation solution)

--- easy/sign of the product of an array/solution.py
+++ easy/sign of the product of an array/solution.py
@@ -3,32 +3,44 @@ from typing import List
 
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        neg = 1
+        result = 0
         for i in nums:
             if i == 0:
                 return 0
-            if i < 0:
-                neg = -1 * neg
-        return neg
+            else:
+                result ^= i
+        return result >> 31 | 1
 
-    def arraySign2(self, nums: List[int]) -> int:
-        neg = 0
-        for i in nums:
-            if i == 0:
-                return 0
-            if i < 0:
-                neg += 1
-        return -1 if (neg % 2 != 0) else 1
 
-    def arraySign1(self, nums: List[int]) -> int:
-        mul = 1
-        for i in nums:
-            mul *= i
-        if mul > 0:
-            return 1
-        elif mul == 0:
+def arraySign3(self, nums: List[int]) -> int:
+    neg = 1
+    for i in nums:
+        if i == 0:
             return 0
-        return -1
+        if i < 0:
+            neg = -1 * neg
+    return neg
+
+
+def arraySign2(self, nums: List[int]) -> int:
+    neg = 0
+    for i in nums:
+        if i == 0:
+            return 0
+        if i < 0:
+            neg += 1
+    return -1 if (neg % 2 != 0) else 1
+
+
+def arraySign1(self, nums: List[int]) -> int:
+    mul = 1
+    for i in nums:
+        mul *= i
+    if mul > 0:
+        return 1
+    elif mul == 0:
+        return 0
+    return -1
 
 
 s = Solution()
