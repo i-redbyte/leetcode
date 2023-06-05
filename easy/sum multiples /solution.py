27d7a6f769f0a51349e92be2ce2253f913bfa026
Ilya Sokolov <developer.sokolov@gmail.com>
2023-06-05 23:34:36 +0300

    @solution: Sum Multiples

---
+++ easy/sum multiples /solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        result = 0
+        for i in range(1, n + 1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                result += i
+        return result
+
+
+s = Solution()
+print(s.sumOfMultiples(7))
+print(s.sumOfMultiples(10))
+print(s.sumOfMultiples(9))
