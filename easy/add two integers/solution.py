59ea3b2f9de70497ba2eb3276cdba4ed40b5a95b
Ilya Sokolov <developer.sokolov@gmail.com>
2023-06-23 18:05:55 +0300

    @solution: Add Two Integers (bitwise solution)

--- easy/add two integers/solution.py
+++ easy/add two integers/solution.py
@@ -1,7 +1,16 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
+        mask = 0xFFFFFFFF
+        if num2 == 0:
+            return num1 if num1 < 0x80000000 else ~(num1 ^ mask)
+        carry = ((num1 & num2) << 1) & mask
+        non_carry = (num1 ^ num2) & mask
+        return self.sum(non_carry, carry)
+
+    def sum1(self, num1: int, num2: int) -> int:
         return num1 + num2
 
 
 print(Solution().sum(12, 5))
 print(Solution().sum(-10, 4))
+print(Solution().sum(-100, -100))
