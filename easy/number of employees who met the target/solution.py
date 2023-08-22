3f53734bde7b38465db9f28ee82e1f0de82342fc
Ilya Sokolov <developer.sokolov@gmail.com>
2023-08-22 23:10:20 +0300

    @solution: Number of Employees Who Met the Target

---
+++ easy/number of employees who met the target/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        n = len(hours) - 1
+        half = n // 2
+        i = 0
+        result = 0
+        while i <= half:
+            if hours[n - i] >= target:
+                result += 1
+            if hours[i] >= target and i != n-i:
+                result += 1
+            i += 1
+        return result
+
+
+s = Solution()
+print(s.numberOfEmployeesWhoMetTarget(hours=[0, 1, 2, 3, 4], target=2))
+print(s.numberOfEmployeesWhoMetTarget(hours=[5, 1, 4, 2, 2], target=6))
