8b470e8976625663b2b9e28c4fb86f672602fe58
Ilya Sokolov <developer.sokolov@gmail.com>
2023-08-29 19:07:52 +0300

    @solution: Number of Common Factors (prime factorization and GCD)

--- easy/number of common factors/solution.py
+++ easy/number of common factors/solution.py
@@ -1,8 +1,24 @@
 class Solution:
-    def commonFactors(self, a: int, b: int) -> int:
-        m = max(a, b)
+    def commonFactors(self, a, b):
+        while b:
+            a, b = b, a % b
+        result = 1
+        div = 2
+        while a != 1 and div * div <= a:
+            count = 0
+            while a % div == 0:
+                count += 1
+                a //= div
+            result *= count + 1
+            div += 1
+        if a != 1:
+            result *= 2
+        return result
+
+    def commonFactors1(self, a: int, b: int) -> int:
+        m = min(a, b)
         result = 0
-        for i in range(1, m+1):
+        for i in range(1, m + 1):
             if a % i == 0 and b % i == 0:
                 result += 1
         return result
