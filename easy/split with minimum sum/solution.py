4fb837aa7c924131dc1fe82b6d2743f71a7c5488
Ilya Sokolov <developer.sokolov@gmail.com>
2023-09-09 12:57:09 +0300

    @solution: Split With Minimum Sum (simple solution)

--- easy/split with minimum sum/solution.py
+++ easy/split with minimum sum/solution.py
@@ -1,5 +1,17 @@
 class Solution:
     def splitNum(self, num: int) -> int:
+        arr = [int(x) for x in str(num)]
+        arr.sort()
+        num1 = 0
+        num2 = 0
+        for i in range(0, len(arr)):
+            if i % 2 == 0:
+                num1 = num1 * 10 + arr[i]
+            elif i % 2 != 0:
+                num2 = num2 * 10 + arr[i]
+        return num1 + num2
+
+    def splitNum1(self, num: int) -> int:
         s = sorted(str(num))
         return int("".join(s[::2])) + int("".join(s[1::2]))
 
