53928f055014d240effbfb23cf4ee207ea66be9b
Ilya Sokolov <developer.sokolov@gmail.com>
2023-10-24 21:41:26 +0300

    @solution: Teemo Attacking (second solution)

--- easy/teemo attacking/solution.py
+++ easy/teemo attacking/solution.py
@@ -3,6 +3,20 @@ from typing import List
 
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        tmp = []
+        for val in timeSeries:
+            tmp.append((val, 1))
+            tmp.append((val + duration, -1))
+        result, cur, start = 0, 0, -1
+        for val, sign in sorted(tmp):
+            if start == -1:
+                start = val
+            cur += sign
+            if not cur:
+                result, start = result + val - start, -1
+        return result
+
+    def findPoisonedDuration1(self, timeSeries: List[int], duration: int) -> int:
         result = 0
         n = len(timeSeries) - 1
         for i in range(n):
