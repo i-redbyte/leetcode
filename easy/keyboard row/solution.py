8342ee24d6ac4408a8da72b201681b910c6f609f
Ilya Sokolov <developer.sokolov@gmail.com>
2024-01-11 00:03:14 +0300

    @solution: Keyboard row (second solution)

--- easy/keyboard row/solution.py
+++ easy/keyboard row/solution.py
@@ -3,6 +3,23 @@ from typing import List
 
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
+        def bitset(word):
+            r = 0
+            for c in word:
+                r |= 1 << (ord(c) - 97)
+            return r
+
+        row1 = bitset('qwertyuiop')
+        row2 = bitset('asdfghjkl')
+        row3 = bitset('zxcvbnm')
+        result = []
+        for word in words:
+            s = bitset(word.casefold())
+            if s & (row1 | row2) < 1 or s & (row1 | row3) < 1 or s & (row2 | row3) < 1:
+                result.append(word)
+        return result
+
+    def findWords1(self, words: List[str]) -> List[str]:
         rows = [set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm")]
         result = []
         for w in words:
