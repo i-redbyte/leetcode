fcd70c4b6606d8fa1aaa67b09f3aee001dda463f
Ilya Sokolov <developer.sokolov@gmail.com>
2024-01-18 22:55:58 +0300

    @solution: Detect Capital (second solution)

--- easy/detect capital/solution.py
+++ easy/detect capital/solution.py
@@ -1,5 +1,19 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
+        up_set = set("QWERTYUIOPLKJHGFDSAZXCVBNM")
+        low_set = set("qwertyuioplkjhgfdsazxcvbnm")
+        word_set = set(word)
+        sub_set = word_set - up_set
+        n = len(sub_set)
+        w = len(word_set)
+        if not n or n == w:
+            return True
+        if word[0] in up_set:
+            if not set(word[1:]) - low_set:
+                return True
+        return False
+
+    def detectCapitalUse1(self, word: str) -> bool:
         counter = 0
         n = len(word)
         for c in word:
