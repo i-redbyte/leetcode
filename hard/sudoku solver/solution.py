b97be3b4e18f239ef3e47e5e58b1d3db64bc7204
Ilya Sokolov <developer.sokolov@gmail.com>
2024-01-21 16:21:11 +0300

    @solution: Sudoku Solver (bitwise version)

--- hard/sudoku solver/solution.py
+++ hard/sudoku solver/solution.py
@@ -3,17 +3,58 @@ from typing import List
 
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
+        rows, cols, boxes = [0] * 9, [0] * 9, [0] * 9
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != '.':
+                    val = int(board[r][c]) - 1
+                    rows[r] |= (1 << val)
+                    cols[c] |= (1 << val)
+                    boxes[(r // 3) * 3 + c // 3] |= (1 << val)
+
+        def solve():
+            empty = None
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == '.':
+                        empty = (r, c)
+                        break
+                if empty:
+                    break
+
+            if not empty:
+                return True
+
+            r, c = empty
+            box_index = (r // 3) * 3 + c // 3
+            for num in range(9):
+                if not (rows[r] & (1 << num)) and not (cols[c] & (1 << num)) and not (boxes[box_index] & (1 << num)):
+                    board[r][c] = str(num + 1)
+                    rows[r] |= (1 << num)
+                    cols[c] |= (1 << num)
+                    boxes[box_index] |= (1 << num)
+                    if solve():
+                        return True
+                    board[r][c] = '.'
+                    rows[r] &= ~(1 << num)
+                    cols[c] &= ~(1 << num)
+                    boxes[box_index] &= ~(1 << num)
+            return False
+
+        solve()
+        return
+
+    def solveSudoku1(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
 
         def is_valid(row, col, num):
-            # Check if the number is not in the given row and column
             for i in range(9):
                 if board[row][i] == num or board[i][col] == num:
                     return False
 
-            # Check if the number is not in the given 3x3 box
             startRow, startCol = 3 * (row // 3), 3 * (col // 3)
             for i in range(3):
                 for j in range(3):
@@ -22,19 +63,17 @@ class Solution:
             return True
 
         def solve():
-            # Find the first empty cell
             for row in range(9):
                 for col in range(9):
                     if board[row][col] == '.':
-                        # Try placing numbers 1-9 in the empty cell
                         for num in '123456789':
                             if is_valid(row, col, num):
                                 board[row][col] = num
-                                if solve():  # Continue with this placement
+                                if solve():
                                     return True
-                                board[row][col] = '.'  # Backtrack
-                        return False  # No valid number found, need to backtrack
-            return True  # All cells filled
+                                board[row][col] = '.'
+                        return False
+            return True
 
         solve()
         return
