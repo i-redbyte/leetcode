28eab0fe9532958197be715cc7f8951ab9065419
Ilya Sokolov <developer.sokolov@gmail.com>
2024-02-06 02:37:55 +0300

    @solution:  Flipping an Image (second solution)

--- easy/flipping an image/solution.py
+++ easy/flipping an image/solution.py
@@ -3,6 +3,18 @@ from typing import List
 
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        n = len(image[0])
+        for row in image:
+            for i in range(n // 2):
+                temp = row[i]
+                row[i] = row[n - 1 - i]
+                row[n - 1 - i] = temp
+
+            for i in range(n):
+                row[i] = 1 - row[i]
+        return image
+
+    def flipAndInvertImage1(self, image: List[List[int]]) -> List[List[int]]:
         return [[1 ^ i for i in row[::-1]] for row in image]
 
 
