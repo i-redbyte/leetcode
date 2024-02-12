4974f368e4561e5ab5363a86b12782e4006f45d6
Ilya Sokolov <developer.sokolov@gmail.com>
2024-02-13 02:49:39 +0300

    @solution: Two Out of Three (second solution)

--- easy/two out of three/solution.py
+++ easy/two out of three/solution.py
@@ -3,6 +3,20 @@ from typing import List
 
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        counter = [0] * 101
+        result = []
+        for i in nums1:
+            counter[i] |= 1
+        for i in nums2:
+            counter[i] |= 1 << 1
+        for i in nums3:
+            counter[i] |= 1 << 2
+        for idx, e in enumerate(counter):
+            if e == 3 or e >= 5:
+                result.append(idx)
+        return result
+
+    def twoOutOfThree1(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         s1, s2, s3 = set(nums1), set(nums2), set(nums3)
         i1 = s1.intersection(s2)
         i2 = s1.intersection(s3)
