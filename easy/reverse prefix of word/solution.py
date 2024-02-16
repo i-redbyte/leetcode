24164cf107244a9e9828cd5c998fd1cf3b818bad
Ilya Sokolov <developer.sokolov@gmail.com>
2024-02-16 12:22:11 +0300

    @solution: Reverse Prefix of Word (bitwise solution)

--- easy/reverse prefix of word/solution.py
+++ easy/reverse prefix of word/solution.py
@@ -1,5 +1,24 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        mask = 0
+        i = 0
+        while i < len(word) and word[i] != ch:
+            mask |= 1 << i
+            i += 1
+
+        if i == len(word):
+            return word
+
+        mask |= 1 << i
+        result = ""
+        for j in range(len(word)):
+            if mask & (1 << j):
+                result += word[i - j]
+            else:
+                result += word[j]
+        return result
+
+    def reversePrefix1(self, word: str, ch: str) -> str:
         index = -1
         for i, symbol in enumerate(word):
             if symbol == ch:
