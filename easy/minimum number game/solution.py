0a6adb3062cbcb5dfb452f77828e2fc7cefb0960
Ilya Sokolov <developer.sokolov@gmail.com>
2024-02-21 23:01:38 +0300

    @solution: Minimum Number Game (second solution)

--- easy/minimum number game/solution.py
+++ easy/minimum number game/solution.py
@@ -3,6 +3,13 @@ from typing import List
 
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        n = len(nums) - 1
+        for i in range(0, n, 2):
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        return nums
+
+    def numberGame1(self, nums: List[int]) -> List[int]:
         result = []
         while nums:
             m1 = min(nums)
