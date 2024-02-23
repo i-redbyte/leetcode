a1c14b5ea4ae958e0afd355cd27b2a84e8d03500
Ilya Sokolov <developer.sokolov@gmail.com>
2024-02-23 22:55:30 +0300

    @solution: Neither Minimum nor Maximum

---
+++ easy/neither minimum nor maximum/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return -1
+        min_el = min(nums)
+        max_el = max(nums)
+        for x in nums:
+            if x != min_el and x != max_el:
+                return x
+        return -1
+
+
+s = Solution()
+print(s.findNonMinOrMax([3, 2, 1, 4]))
+print(s.findNonMinOrMax([1, 2]))
+print(s.findNonMinOrMax([2, 1, 3]))
