b2d09bd0ad04465e5725c7a5fe4a3886208d2687
Ilya Sokolov <developer.sokolov@gmail.com>
2024-07-15 21:19:47 +0300

    @solution: 1-bit and 2-bit Characters

---
+++ easy/1-bit and 2-bit characters/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        n = len(bits)
+        while i < n - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        return i == n - 1
+
+
+s = Solution()
+print(s.isOneBitCharacter([1, 0, 0]))
+print(s.isOneBitCharacter([1, 1, 1, 0]))
