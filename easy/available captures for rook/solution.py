e59ac0bdc89cdcfb125dda43ff280f5ea423c222
Ilya Sokolov <developer.sokolov@gmail.com>
2024-09-27 13:56:24 +0300

    @solution: Available Captures for Rook (bitwise version)

--- easy/available captures for rook/solution.py
+++ easy/available captures for rook/solution.py
@@ -3,6 +3,48 @@ from typing import List
 
 class Solution:
     def numRookCaptures(self, board: List[List[str]]) -> int:
+        rook_position = 0
+        pawns = 0
+        bishops = 0
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                bit_position: int = (i * 8 + j)
+                if board[i][j] == 'R':
+                    rook_position |= (1 << bit_position)
+                elif board[i][j] == 'p':
+                    pawns |= (1 << bit_position)
+                elif board[i][j] == 'B':
+                    bishops |= (1 << bit_position)
+
+        result = 0
+
+        def check_direction(shift: int, mask_limit: int) -> None:
+            nonlocal result
+            mask: int = rook_position
+            while True:
+                mask = (mask << shift) if shift > 0 else (mask >> -shift)
+
+                if mask & mask_limit == 0:
+                    break
+                if mask & (pawns | bishops):
+                    if mask & pawns:
+                        result += 1
+                    break
+                if mask == 0:
+                    break
+
+        row_mask = 0xFF
+        current_row_mask = row_mask << ((rook_position.bit_length() // 8) * 8)
+
+        check_direction(1, current_row_mask)
+        check_direction(-1, current_row_mask)
+        check_direction(8, 0xFFFFFFFFFFFFFFFF)
+        check_direction(-8, 0xFFFFFFFFFFFFFFFF)
+
+        return result
+
+    def numRookCaptures1(self, board: List[List[str]]) -> int:
         rook_row = 0
         rook_col = 0
         result = 0
