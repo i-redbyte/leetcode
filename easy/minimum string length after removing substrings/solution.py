10ae0059cdfba5e5e3909de6bcfa4bec1a0a156e
Ilya Sokolov <developer.sokolov@gmail.com>
2024-10-07 22:39:25 +0300

    @solution: Minimum String Length After Removing Substrings (recursive solution)

--- easy/minimum string length after removing substrings/solution.py
+++ easy/minimum string length after removing substrings/solution.py
@@ -1,5 +1,9 @@
 class Solution:
     def minLength(self, s: str) -> int:
+        new_string = s.replace("AB", "").replace("CD", "")
+        return len(new_string) if new_string == s else self.minLength(new_string)
+
+    def minLength1(self, s: str) -> int:
         stack = []
         for char in s:
             if stack and ((stack[-1] == 'A' and char == 'B') or (stack[-1] == 'C' and char == 'D')):
