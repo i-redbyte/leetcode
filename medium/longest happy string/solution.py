9aeb1c3820f783634892357e89a5bce5858c23b5
Ilya Sokolov <developer.sokolov@gmail.com>
2024-10-16 19:08:26 +0300

    @solution: Longest Happy String (solution two: with use graph theory)

--- medium/longest happy string/solution.py
+++ medium/longest happy string/solution.py
@@ -1,9 +1,36 @@
 from heapq import heappush, heappop
-from typing import Tuple
+from collections import deque
+from typing import Dict, List, Tuple
 
 
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        graph: Dict[str, int] = {'a': a, 'b': b, 'c': c}
+        queue = deque(sorted([(count, char) for char, count in graph.items() if count > 0], key=lambda x: -x[0]))
+        result = []
+
+        while queue:
+            count1, char1 = queue.popleft()
+            if len(result) >= 2 and result[-1] == result[-2] == char1:
+                if not queue:
+                    break
+                count2, char2 = queue.popleft()
+                result.append(char2)
+                count2 -= 1
+                if count2 > 0:
+                    queue.append((count2, char2))
+                queue.append((count1, char1))
+            else:
+                result.append(char1)
+                count1 -= 1
+                if count1 > 0:
+                    queue.append((count1, char1))
+
+            queue = deque(sorted(queue, key=lambda x: -x[0]))
+
+        return "".join(result)
+
+    def longestDiverseString1(self, a: int, b: int, c: int) -> str:
         max_heap: list[Tuple[int, str]] = []
         if a > 0:
             heappush(max_heap, (-a, 'a'))
