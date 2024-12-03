34acaa0748c4974926cb0b777abbd6b3862ad7e8
Ilya Sokolov <developer.sokolov@gmail.com>
2024-12-03 20:10:53 +0300

    @badsolution: Adding Spaces to a String

---
+++ hard/adding spaces to a string/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        result = ""
+        for i, ch in enumerate(s):
+            if i in spaces:
+                result += " "
+            result += ch
+        return result
+
+
+s = Solution()
+
+print(s.addSpaces(s="LeetcodeHelpsMeLearn", spaces=[8, 13, 15]))
+print(s.addSpaces(s="icodeinpython", spaces=[1, 5, 7, 9]))
+print(s.addSpaces(s="spacing", spaces=[0, 1, 2, 3, 4, 5, 6]))
