a2fbc5d8235092879c78697042fe7d74dc3896ee
Ilya Sokolov <developer.sokolov@gmail.com>
2025-02-22 15:24:14 +0300

    update utils (add ListNode)

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 def printTree(node: 'TreeNode', level=0):
