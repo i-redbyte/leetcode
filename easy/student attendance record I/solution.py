6302c14d6646d1f0c7b4fb072477091dded8d8f8
Ilya Sokolov <developer.sokolov@gmail.com>
2025-09-05 20:09:48 +0300

    @rust: Student Attendance Record I (bitwise version)

--- easy/student attendance record I/solution.py
+++ easy/student attendance record I/solution.py
@@ -1,5 +1,31 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
+        state = 0  # bits 0-1: counter A, bits 2-3: counter L
+        for char in s:
+            if char == 'A':
+                a_count = (state & 0b11) + 1
+                if a_count > 2:
+                    a_count = 2
+                state = (state & 0b11111100) | a_count
+                state &= 0b00000011
+
+            elif char == 'L':
+                l_count = ((state >> 2) & 0b11) + 1
+                if l_count > 3:
+                    l_count = 3
+                state = (state & 0b11110011) | (l_count << 2)
+
+            else:  # 'P'
+                state &= 0b00000011
+
+            if (state & 0b11) >= 2:
+                return False
+            if ((state >> 2) & 0b11) >= 3:
+                return False
+
+        return True
+
+    def checkRecord1(self, s: str) -> bool:
         a = 0
         l = 0
         for ch in s:
@@ -22,3 +48,4 @@ s = Solution()
 
 print(s.checkRecord("PPALLP"))
 print(s.checkRecord("PPALLL"))
+print(s.checkRecord("AAAA"))
