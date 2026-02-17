f9437582dbb0b4c588cbaf2a5642a2cf027b8dd3
Ilya Sokolov <developer.sokolov@gmail.com>
2026-02-17 22:49:08 +0300

    @solution: Binary watch (add new solution)

--- easy/binary watch/solution.py
+++ easy/binary watch/solution.py
@@ -10,12 +10,22 @@ class Solution:
             n >>= 1
         return result
 
-    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+    def readBinaryWatch1(self, turnedOn: int) -> List[str]:
         result = []
         for h in range(12):
             for m in range(60):
                 if self.bit_counter(h) + self.bit_counter(m) == turnedOn:
                     result.append(f'{h}:{m:02}')
+        return result
+
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        bits_h = [bin(h).count("1") for h in range(12)]
+        bits_m = [bin(m).count("1") for m in range(60)]
+        result = []
+        for h in range(12):
+            for m in range(60):
+                if bits_h[h] + bits_m[m] == turnedOn:
+                    result.append(f"{h}:{m:02d}")
         return result
 
 
