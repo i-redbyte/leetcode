73d2e2719ef83ef490cab67546b383d96c636abb
Ilya Sokolov <developer.sokolov@gmail.com>
2026-03-01 18:33:54 +0300

    @solution: Trionic Array I

---
+++ easy/trionic array i/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        n = len(nums)
+        p = 0
+        while p < n - 2 and nums[p] < nums[p + 1]:
+            p += 1
+        if p == 0:
+            return False
+        q = p
+        while q < n - 1 and nums[q] > nums[q + 1]:
+            q += 1
+        if q == p or q == n - 1:
+            return False
+        while q < n - 1 and nums[q] < nums[q + 1]:
+            q += 1
+        return q == n - 1
+
+
+s = Solution()
+print(s.isTrionic([1, 3, 5, 4, 2, 6]))
+print(s.isTrionic([2, 1, 3]))
